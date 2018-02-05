85f05799d4bdc62ffb2a0fb49671b2f675c85c9d
Mingkun <hmkhaha@gmail.com>
2018-02-04 21:58:19 -0700

    Initial Commit

---
+++ 599proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  599proto
+//
+//  Created by mingkun he on 2018-02-04.
+//  Copyright © 2018 mingkun he. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
